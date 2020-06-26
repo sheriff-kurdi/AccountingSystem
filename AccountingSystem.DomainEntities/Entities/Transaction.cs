9577e7c204c8a0d9a0a6b8aef5f75eae95eecb81
sheriff-kurdi <sheriff.kurdi@gmail.com>
2020-06-26 10:43:14 +0200

    Add project files.

---
+++ AccountingSystem.DomainEntities/Entities/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountingSystem.DomainEntities.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Cash { get; set; }
+        public double AccountReceivable { get; set; }
+        public double AccountPyable { get; set; }
+        public double Equipment { get; set; }
+        public double Land { get; set; }
+        public double Building { get; set; }
+        public double Goods { get; set; }
+    
+
+    }
+}
---
+++ AccountingSystem.DomainEntities/Enums/PossibleTransactions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountingSystem.DomainEntities.Enums
+{
+    public enum PossibleTransactions
+    {
+        Cash,
+        Building,
+        Land,
+        Equipments,
+        Goods
+    }
+}
