52d1d3d4bbb0384f9824fec6cf178f337bc40cb2
sheriff-kurdi <sheriff.kurdi@gmail.com>
2020-06-28 22:44:26 +0200

    BalanceSheetv1

--- AccountingSystem.Service/Balances/AccountingCalcForBalance.cs
+++ AccountingSystem.Service/Balances/AccountingCalcForBalance.cs
@@ -15,49 +15,49 @@ namespace AccountingSystem.Service.Balances
             //Assets
             if (transaction.CrTransactionEntry == PossibleTransactions.AccountRecevable)
             {
-                lastBalance.AccountRecevable -= transaction.DrTransactionEntryValue;
+                lastBalance.AccountRecevable -= transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.Building)
             {
-                lastBalance.Building -= transaction.DrTransactionEntryValue;
+                lastBalance.Building -= transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.Cash)
             {
-                lastBalance.Cash -= transaction.DrTransactionEntryValue;
+                lastBalance.Cash -= transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.Equipments)
             {
-                lastBalance.Equipments -= transaction.DrTransactionEntryValue;
+                lastBalance.Equipments -= transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.Goods)
             {
-                lastBalance.Goods -= transaction.DrTransactionEntryValue;
+                lastBalance.Goods -= transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.Land)
             {
-                lastBalance.Land -= transaction.DrTransactionEntryValue;
+                lastBalance.Land -= transaction.CrTransactionEntryValue;
             }
 
 
             //Liabilites
             if (transaction.CrTransactionEntry == PossibleTransactions.AccountPyable)
             {
-                lastBalance.AccountPyable += transaction.DrTransactionEntryValue;
+                lastBalance.AccountPyable += transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.Loans)
             {
-                lastBalance.Loans += transaction.DrTransactionEntryValue;
+                lastBalance.Loans += transaction.CrTransactionEntryValue;
             }
             if (transaction.CrTransactionEntry == PossibleTransactions.SalaryPyable)
             {
-                lastBalance.SalaryPyable += transaction.DrTransactionEntryValue;
+                lastBalance.SalaryPyable += transaction.CrTransactionEntryValue;
             }
 
 
             //Owner's Equity
             if (transaction.CrTransactionEntry == PossibleTransactions.Capital)
             {
-                lastBalance.Capital += transaction.DrTransactionEntryValue;
+                lastBalance.Capital += transaction.CrTransactionEntryValue;
             }
 
             return lastBalance;
@@ -126,21 +126,19 @@ namespace AccountingSystem.Service.Balances
             return balance;
         }
 
-        public Balance handleAccPyapleAndAccRecevable(Balance balance, Transaction transaction)
-        {
-
-            balance.AccountPyable = transaction.AccountPyable;
-            balance.AccountRecevable = transaction.AccountReceivable;
-            return balance;
-        }
+    
 
         public Balance EditLastBalance(Transaction transaction, Balance lastBalance)
         {
             lastBalance.Date = DateTime.Now;
 
+            // if you wanna edit balance in every transaction
+            lastBalance = HandleDebitEntriesForBalance(transaction, lastBalance);
+            lastBalance = HandleCreditEntriesForBalance(transaction, lastBalance);
+
             //handle A/P & A/R
-            lastBalance.AccountPyable = handleAccPyapleAndAccRecevable(lastBalance, transaction).AccountPyable;
-            lastBalance.AccountRecevable = handleAccPyapleAndAccRecevable(lastBalance, transaction).AccountRecevable;
+            lastBalance.AccountPyable += transaction.AccountPyable;
+            lastBalance.AccountRecevable += transaction.AccountReceivable;
             //.....
 
             // calc balace Totall
@@ -148,9 +146,7 @@ namespace AccountingSystem.Service.Balances
             lastBalance.TotallLiabilityAndEquity = CalculateTotalBalance(lastBalance).TotallLiabilityAndEquity;
             //...............
 
-            // if you wanna edit balance in every transaction
-            lastBalance =  HandleDebitEntriesForBalance(transaction, lastBalance);
-            lastBalance= HandleCreditEntriesForBalance(transaction, lastBalance);
+          
 
             //if you wanna create balance for whole old transactions
 
